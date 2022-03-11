0227bef8b046f4e7ce66173016539309f216869a
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-03-11 22:11:16 +0100

    feature(region): add service

--- src/services/mod.ts
+++ src/services/mod.ts
@@ -3,6 +3,7 @@ import DefaultCartService from "./cartService/mod.ts";
 import DefaultOrderService from "./orderService/mod.ts";
 import DefaultCategoryService from "./categoryService/mod.ts";
 import DefaultCategoryLinkService from "./categoryLinkService/mod.ts";
+import DefaultRegionService from "./regionService/mod.ts";
 
 export const DataService: ProductService = new ProductService();
 export const CartService: DefaultCartService = new DefaultCartService();
@@ -11,3 +12,4 @@ export const CategoryService: DefaultCategoryService =
   new DefaultCategoryService();
 export const CategoryLinkService: DefaultCategoryLinkService =
   new DefaultCategoryLinkService();
+export const RegionService: DefaultRegionService = new DefaultRegionService();
---
+++ src/services/regionService/mod.ts
@@ -0,0 +1,92 @@
+import client from "../dataClient/client.ts";
+import type postgresClient from "../dataClient/client.ts";
+import IRegionService from "../interfaces/regionService.ts";
+import { Region } from "../../utils/types.ts";
+
+export default class RegionService implements IRegionService {
+  client: typeof postgresClient;
+  constructor() {
+    this.client = client;
+  }
+
+  async Create(params: { data: Region }): Promise<Region> {
+    try {
+      await this.client.connect();
+
+      const result = await this.client.queryObject<Region>({
+        text:
+          "INSERT INTO regions (name, currency, regions) VALUES ($1, $2, $3)",
+        args: [params.data.name, params.data.currency, params.data.regions],
+      });
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error("DB error", {
+        cause: error,
+      });
+    } finally {
+      await this.client.end();
+    }
+  }
+
+  async Get(params: { id: string }): Promise<Region> {
+    try {
+      await this.client.connect();
+
+      const result = await this.client.queryObject<Region>({
+        text: "SELECT * FROM regions WHERE id = $1 LIMIT 1",
+        args: [params.id],
+      });
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error("DB error", {
+        cause: error,
+      });
+    } finally {
+      await this.client.end();
+    }
+  }
+
+  async Update(params: { data: Region }): Promise<Region> {
+    try {
+      await this.client.connect();
+
+      const result = await this.client.queryObject<Region>({
+        text:
+          "UPDATE regions WHERE SET name = $1, currency = $2, regions = $3  WHERE id = $3",
+        args: [
+          params.data.name,
+          params.data.currency,
+          params.data.regions,
+          params.data.id,
+        ],
+      });
+
+      return result.rows[0];
+    } catch (error) {
+      throw new Error("DB error", {
+        cause: error,
+      });
+    } finally {
+      await this.client.end();
+    }
+  }
+
+  async Delete(params: { id: string }): Promise<void> {
+    try {
+      await this.client.connect();
+
+      await this.client.queryObject<void>({
+        text: "DELETE FROM regions WHERE id = $1",
+        args: [params.id],
+      });
+    } catch (error) {
+      throw new Error("DB error", {
+        cause: error,
+      });
+    } finally {
+      await this.client.end();
+    }
+  }
+}
