ce4211afb20958ebe21869fc4dedef8e1ae872d3
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-03-14 19:49:45 +0100

    feat(routes): update how routes are handled

--- src/routes/api/carts.ts
+++ src/routes/api/carts.ts
@@ -1,61 +1,70 @@
 import { Router } from "../../deps.ts";
+import ICartService from "../../services/interfaces/cartService.ts";
 
-import { CartService } from "../../services/mod.ts";
 import { stringifyJSON } from "../../utils/utils.ts";
 
-const carts = new Router({
-  prefix: "/carts",
-});
+export default class CartRoutes {
+  #carts: Router;
+  #CartService: ICartService;
+  constructor(cartService: ICartService) {
+    this.#CartService = cartService;
+    this.#carts = new Router({
+      prefix: "/carts",
+    });
 
-carts.post("/", async (ctx) => {
-  try {
-    const data = await CartService.Create({
-      data: {
-        id: 1,
-        created_at: new Date().getUTCDate(),
-        products: [{
-          pid: "234",
-          quantity: 3,
-        }],
-      },
+    this.#carts.post("/", async (ctx) => {
+      try {
+        const data = await this.#CartService.Create({
+          data: {
+            id: 1,
+            created_at: new Date().getUTCDate(),
+            products: [{
+              pid: "234",
+              quantity: 3,
+            }],
+          },
+        });
+        ctx.response.body = stringifyJSON({
+          carts: data,
+        });
+      } catch (error) {
+        ctx.response.body = JSON.stringify(error);
+      }
     });
-    ctx.response.body = stringifyJSON({
-      carts: data,
+
+    this.#carts.get("/:id", async (ctx) => {
+      try {
+        const data = await this.#CartService.Get({ id: ctx.params.id });
+        ctx.response.body = stringifyJSON({
+          carts: data,
+        });
+      } catch (error) {
+        ctx.response.body = JSON.stringify(error);
+      }
     });
-  } catch (error) {
-    ctx.response.body = JSON.stringify(error);
+
+    this.#carts.put("/:id", async (ctx) => {
+      try {
+        const data = await this.#CartService.Update({
+          data: {
+            id: parseInt(ctx.params.id),
+            created_at: new Date().getUTCDate(),
+            products: [{
+              pid: "234",
+              quantity: 3,
+            }],
+          },
+        });
+        ctx.response.body = stringifyJSON({
+          carts: data,
+        });
+      } catch (error) {
+        ctx.response.body = JSON.stringify(error);
+      }
+    });
   }
-});
 
-carts.get("/:id", async (ctx) => {
-  try {
-    const data = await CartService.Get({ id: ctx.params.id });
-    ctx.response.body = stringifyJSON({
-      carts: data,
-    });
-  } catch (error) {
-    ctx.response.body = JSON.stringify(error);
+  routes() {
+    return this.#carts.routes();
   }
-});
-
-carts.put("/:id", async (ctx) => {
-  try {
-    const data = await CartService.Update({
-      data: {
-        id: parseInt(ctx.params.id),
-        created_at: new Date().getUTCDate(),
-        products: [{
-          pid: "234",
-          quantity: 3,
-        }],
-      },
-    });
-    ctx.response.body = stringifyJSON({
-      carts: data,
-    });
-  } catch (error) {
-    ctx.response.body = JSON.stringify(error);
-  }
-});
-
-export default carts;
+}
--- src/routes/api/orders.ts
+++ src/routes/api/orders.ts
@@ -1,32 +1,41 @@
 import { Router } from "../../deps.ts";
+import IOrderService from "../../services/interfaces/orderService.ts";
 
-import { OrderService } from "../../services/mod.ts";
 import { stringifyJSON } from "../../utils/utils.ts";
 
-const orders = new Router({
-  prefix: "/orders",
-});
-
-orders.get("/", async (ctx) => {
-  try {
-    const data = await OrderService.GetMany({});
-    ctx.response.body = stringifyJSON({
-      orders: data,
+export default class OrdersRoutes {
+  #orders: Router;
+  #OrderService: IOrderService;
+  constructor(orderService: IOrderService) {
+    this.#OrderService = orderService;
+    this.#orders = new Router({
+      prefix: "/orders",
     });
-  } catch (error) {
-    ctx.response.body = JSON.stringify(error);
-  }
-});
 
-orders.get("/:id", async (ctx) => {
-  try {
-    const data = await OrderService.Get({ id: ctx.params.id });
-    ctx.response.body = stringifyJSON({
-      orders: data,
+    this.#orders.get("/", async (ctx) => {
+      try {
+        const data = await this.#OrderService.GetMany({});
+        ctx.response.body = stringifyJSON({
+          orders: data,
+        });
+      } catch (error) {
+        ctx.response.body = JSON.stringify(error);
+      }
     });
-  } catch (error) {
-    ctx.response.body = JSON.stringify(error);
-  }
-});
 
-export default orders;
+    this.#orders.get("/:id", async (ctx) => {
+      try {
+        const data = await this.#OrderService.Get({ id: ctx.params.id });
+        ctx.response.body = stringifyJSON({
+          orders: data,
+        });
+      } catch (error) {
+        ctx.response.body = JSON.stringify(error);
+      }
+    });
+  }
+
+  routes() {
+    return this.#orders.routes();
+  }
+}
