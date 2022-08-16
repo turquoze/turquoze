bf25a4aff15dc0ad00254961f085024b93fed09e
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-08-16 20:53:08 +0200

    remove unused route

--- src/routes/api/carts.ts
+++ src/routes/api/carts.ts
@@ -1,7 +1,6 @@
-import { jwt, Router } from "../../deps.ts";
+import { Router } from "../../deps.ts";
 import Container from "../../services/mod.ts";
 import { ErrorHandler, NoBodyError } from "../../utils/errors.ts";
-import { JWTKEY } from "../../utils/secrets.ts";
 import { Cart, CartItem, DiscountCheck } from "../../utils/types.ts";
 
 import { Delete, Get, stringifyJSON } from "../../utils/utils.ts";
@@ -56,88 +55,6 @@ export default class CartRoutes {
       }
     });
 
-    /*
-    this.#carts.post("/discount", async (ctx) => {
-      try {
-        const token = ctx.request.headers.get("x-cart-token");
-        if (token == undefined) {
-          throw new Error("No token");
-        }
-
-        if (!await jwt.default.verify(token, JWTKEY)) {
-          throw new Error("No valid token");
-        }
-
-        if (!ctx.request.hasBody) {
-          throw new NoBodyError("No Body");
-        }
-
-        const { cartId } = jwt.default.decode(token);
-
-        const body = ctx.request.body();
-        let discountCheck: DiscountCheck;
-        if (body.type === "json") {
-          discountCheck = await body.value;
-        } else {
-          throw new NoBodyError("Wrong content-type");
-        }
-
-        await DiscountCheckSchema.validate(discountCheck);
-        const posted: DiscountCheck = await DiscountCheckSchema.cast(
-          discountCheck,
-        );
-
-        const discount = await this.#Container.DiscountService.GetByCode({
-          code: posted.code,
-        });
-
-        const cart = await this.#Container.CartService.Get({
-          id: cartId,
-        });
-
-        if (discount != undefined) {
-          if (
-            (discount.valid_from ?? 1) <= Date.now() &&
-            Date.now() <= (discount.valid_to ?? Number.MAX_SAFE_INTEGER)
-          ) {
-            let hasDiscount = undefined;
-            if (cart.discounts != null && cart.discounts != undefined) {
-              hasDiscount = cart.discounts?.cart.find((d) =>
-                d.did == discount.public_id
-              );
-            }
-
-            if (hasDiscount == undefined) {
-              cart.discounts = { cart: [{ did: discount.public_id }] };
-            } else {
-              const newDiscountArr = cart.discounts.cart.filter((item) => {
-                if (item.did != discount.public_id) {
-                  return item;
-                }
-              });
-              cart.discounts.cart = newDiscountArr;
-            }
-            await this.#Container.CartService.CreateOrUpdate({
-              data: cart,
-            });
-          }
-        }
-
-        ctx.response.body = stringifyJSON({
-          carts: cart,
-        });
-        ctx.response.headers.set("content-type", "application/json");
-      } catch (error) {
-        const data = ErrorHandler(error);
-        ctx.response.status = data.code;
-        ctx.response.headers.set("content-type", "application/json");
-        ctx.response.body = JSON.stringify({
-          message: data.message,
-        });
-      }
-    });
-    */
-
     this.#carts.post("/:id/finalize", async (ctx) => {
       try {
         await UuidSchema.validate({
@@ -155,43 +72,6 @@ export default class CartRoutes {
         ctx.response.body = stringifyJSON({
           order: order.id,
           payment: order.payment,
-        });
-        ctx.response.headers.set("content-type", "application/json");
-      } catch (error) {
-        const data = ErrorHandler(error);
-        ctx.response.status = data.code;
-        ctx.response.headers.set("content-type", "application/json");
-        ctx.response.body = JSON.stringify({
-          message: data.message,
-        });
-      }
-    });
-
-    this.#carts.post("/:id/init", async (ctx) => {
-      try {
-        await UuidSchema.validate({
-          id: ctx.params.id,
-        });
-
-        /*const cart = await Get<Cart>({
-          id: `cart_${ctx.params.id}`,
-          promise: this.#Container.CartService.Get({
-            id: ctx.params.id,
-          }),
-        });*/
-
-        const cart = await this.#Container.CartService.Get({
-          id: ctx.params.id,
-        });
-
-        const token = await jwt.default.sign({
-          cartId: cart.public_id,
-          nbf: Math.floor(Date.now() / 1000),
-          exp: Math.floor(Date.now() / 1000) + (1 * (60 * 60)),
-        }, JWTKEY);
-
-        ctx.response.body = stringifyJSON({
-          token: token,
         });
         ctx.response.headers.set("content-type", "application/json");
       } catch (error) {
