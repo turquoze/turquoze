3749a95302f9d6b38a2ff02d255452ac349adeaa
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-10-04 21:08:22 +0200

    setup roles

---
+++ src/middleware/roleGuard.ts
@@ -0,0 +1,62 @@
+import type { Context } from "../deps.ts";
+import { TurquozeRole, TurquozeState } from "../utils/types.ts";
+
+export const RoleGuard = (role: TurquozeRole) => {
+  return (
+    ctx: Context<TurquozeState>,
+    next: () => Promise<unknown>,
+  ) => {
+    const userRole: TurquozeRole = "WEBSITE";
+    if (isAllowed(role, userRole)) {
+      next();
+    } else {
+      ctx.response.status = 401;
+      ctx.response.headers.set("content-type", "application/json");
+      ctx.response.body = JSON.stringify({
+        msg: "Not allowed",
+        error: "NO_TOKEN",
+      });
+    }
+  };
+};
+
+function isAllowed(role: TurquozeRole, userRole: TurquozeRole): boolean {
+  if (role == "SUPERADMIN") {
+    if (userRole == "SUPERADMIN") {
+      return true;
+    }
+    return false;
+  } else if (role == "ADMIN") {
+    if (userRole == "SUPERADMIN" || userRole == "ADMIN") {
+      return true;
+    }
+    return false;
+  } else if (role == "WEBSITE") {
+    if (
+      userRole == "SUPERADMIN" || userRole == "ADMIN" || userRole == "WEBSITE"
+    ) {
+      return true;
+    }
+    return false;
+  } else if (role == "VIEWER") {
+    if (
+      userRole == "SUPERADMIN" || userRole == "ADMIN" ||
+      userRole == "WEBSITE" || userRole == "VIEWER"
+    ) {
+      return true;
+    }
+    return false;
+  } else if (role == "USER") {
+    if (
+      userRole == "SUPERADMIN" || userRole == "ADMIN" ||
+      userRole == "WEBSITE" || userRole == "VIEWER" || userRole == "USER"
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  return false;
+}
+
+export default RoleGuard;
