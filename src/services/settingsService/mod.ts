3292825a6f42ead99066a996bd07658cfdf6f43d
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-10-29 16:12:40 +0200

    add settings service

---
+++ src/services/settingsService/mod.ts
@@ -0,0 +1,55 @@
+import ISettingsService from "../interfaces/settingsService.ts";
+import { DatabaseError } from "../../utils/errors.ts";
+import container from "../mod.ts";
+import type { Pool } from "../../deps.ts";
+import type { Settings, Shop } from "../../utils/types.ts";
+
+export default class SettingsService implements ISettingsService {
+  pool: Pool;
+  constructor(pool: Pool) {
+    this.pool = pool;
+  }
+
+  async Upsert(params: { data: Settings; shopId: string }): Promise<void> {
+    try {
+      const client = await this.pool.connect();
+
+      await client.queryObject<Shop>({
+        text:
+          "UPDATE shops SET settings = $1 WHERE public_id = $2 RETURNING public_id",
+        args: [
+          params.data,
+          params.shopId,
+        ],
+      });
+
+      client.release();
+    } catch (error) {
+      throw new DatabaseError("DB error", {
+        cause: error,
+      });
+    }
+  }
+
+  async GetById(params: { id: string }): Promise<string> {
+    const obj = Object.entries(container.Shop.settings).find((x) =>
+      x[0] == params.id
+    );
+
+    if (obj != undefined) {
+      await new Promise((resolve, _reject) => {
+        resolve("");
+      });
+      return obj[1] as string;
+    }
+
+    throw new Error("No setting");
+  }
+
+  async Get(): Promise<Settings> {
+    await new Promise((resolve, _reject) => {
+      resolve("");
+    });
+    return container.Shop.settings;
+  }
+}
