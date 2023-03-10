7a05f0bb220da6a50f782d2bec9dda291c3be714
EduM22 <38257387+EduM22@users.noreply.github.com>
2023-03-10 22:57:06 +0100

    update deps + setup scheme for future

--- src/deps.ts
+++ src/deps.ts
@@ -2,18 +2,18 @@ export {
   Application,
   Context,
   Router,
-} from "https://deno.land/x/oak@v11.1.0/mod.ts";
+} from "https://deno.land/x/oak@v12.1.0/mod.ts";
 export * as yup from "https://cdn.skypack.dev/yup@0.32.11";
 export { Redis } from "https://deno.land/x/upstash_redis@v1.3.2/mod.ts";
-export { MeiliSearch } from "https://esm.sh/meilisearch@0.25.1";
+export { MeiliSearch } from "https://esm.sh/meilisearch@0.31.1";
 export type {
   EnqueuedTask,
   SearchParams,
   SearchResponse,
-} from "https://esm.sh/meilisearch@0.25.1";
+} from "https://esm.sh/meilisearch@0.31.1";
 export * as postgres from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 export type { Pool } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
-export * as jose from "https://deno.land/x/jose@v4.9.1/index.ts";
+export * as jose from "https://deno.land/x/jose@v4.13.1/index.ts";
 export { createClient } from "https://esm.sh/@supabase/supabase-js@1.35.3";
 export type { SupabaseClient } from "https://esm.sh/@supabase/supabase-js@1.35.3";
 export {
