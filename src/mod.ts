dff7183be5ac56211b7f815186d684a0e4e804b1
EduM22 <38257387+EduM22@users.noreply.github.com>
2023-04-13 20:32:52 +0200

    remove old ui + tsx render

--- src/mod.ts
+++ src/mod.ts
@@ -1,15 +1,13 @@
 import app from "./app.ts";
 import admin from "./routes/admin/admin.ts";
 import api from "./routes/api/api.ts";
-import ui from "./routes/ui/ui.ts";
 import utils from "./routes/utils/utils.ts";
 import ResponseTimer from "./middleware/responseTimer.ts";
 import Logger from "./middleware/logger.ts";
 import addEvents from "./utils/events.ts";
 import DBCloser from "./middleware/dbCloser.ts";
 import { stringifyJSON } from "./utils/utils.ts";
-import notFoundPage from "./pages/404.tsx";
-import Render from "./utils/render.ts";
+import notFoundPage from "./pages/404.ts";
 
 addEvents();
 
@@ -17,7 +15,6 @@ app.use(DBCloser);
 app.use(Logger);
 app.use(ResponseTimer);
 
-app.use(ui.routes());
 app.use(admin.routes());
 app.use(api.routes());
 app.use(utils.routes());
@@ -26,9 +23,10 @@ app.use((ctx) => {
   const hit = ctx.request.accepts()?.find((x) => x == "text/html");
 
   if (hit != undefined) {
-    const html = Render(notFoundPage);
+    const html = notFoundPage;
 
     ctx.response.status = 404;
+    ctx.response.headers.set("content-type", "text/html");
     ctx.response.body = html;
   } else {
     ctx.response.status = 404;
---
+++ src/pages/404.ts
@@ -0,0 +1,4 @@
+const NotFound =
+  `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>404 page</title><meta content="404 page" name="description" /><style>.center {text-align: center;}.center > h2 {font-size: 3rem;}.center > p {font-size: 2rem;}</style></head><body><main><div class="center"><h2>404</h2><p>Not Found</p></div></main></body></html>`;
+
+export default NotFound;
