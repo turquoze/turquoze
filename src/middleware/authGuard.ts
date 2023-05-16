2a30344eb42f32cc0b2a472a37ffe05d68903e7d
EduM22 <38257387+EduM22@users.noreply.github.com>
2023-05-16 21:38:55 +0200

    fix jwt data parse error

--- src/middleware/authGuard.ts
+++ src/middleware/authGuard.ts
@@ -35,7 +35,7 @@ export const AuthGuard =
           SHARED_SECRET_KEY,
         );
 
-        const shops = result.payload.shops as Array<ShopLinkData>;
+        const shops = JSON.parse(result.payload.shops as string) as Array<ShopLinkData>;
 
         const match = shops.find((x) => x.public_id == shopId);
 
