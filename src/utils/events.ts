23d9139290140f5e752ab3814e447593a07506b0
EduM22 <38257387+EduM22@users.noreply.github.com>
2024-01-11 21:01:08 +0100

    fix shop undefined in event handler

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,30 +1,47 @@
+import { MeiliSearch } from "meilisearch";
 import type Container from "../services/mod.ts";
+import { Shop } from "./schema.ts";
 
-export function reIndex(id: string, container: Container) {
+export function reIndex(id: string, shop: Shop, container: Container) {
   container.ProductService.Get({ id }).then((product) => {
-    const index = container.Shop.searchIndex;
+    const index = shop.searchIndex;
     const productToIndex = {
       ...product,
     };
     delete productToIndex.publicId;
 
+    const client = new MeiliSearch({
+      //@ts-ignore not on type
+      host: shop.settings.meilisearch.host,
+      //@ts-ignore not on type
+      apiKey: shop.settings.meilisearch.api_key,
+    });
+
     container.SearchService.ProductIndex({
       index: index!,
       //@ts-ignore not on type
       products: [productToIndex],
-    }).then((task) => {
+    }, client).then((task) => {
       console.info(task);
     }).catch((error) => console.log(`could not finish event`, error));
   }).catch((error) => console.log(`could not finish event`, error));
 }
 
-export function removeProduct(id: string, container: Container) {
+export function removeProduct(id: string, shop: Shop, container: Container) {
   container.ProductService.Get({ id }).then((product) => {
-    const index = container.Shop.searchIndex;
+    const index = shop.searchIndex;
+
+    const client = new MeiliSearch({
+      //@ts-ignore not on type
+      host: shop.settings.meilisearch.host,
+      //@ts-ignore not on type
+      apiKey: shop.settings.meilisearch.api_key,
+    });
+
     container.SearchService.ProductRemove({
       index: index!,
       id: product.publicId!.toString(),
-    }).then((task) => {
+    }, client).then((task) => {
       console.info(task);
     }).catch((error) => console.log(`could not finish event`, error));
   }).catch((error) => console.log(`could not finish event`, error));
